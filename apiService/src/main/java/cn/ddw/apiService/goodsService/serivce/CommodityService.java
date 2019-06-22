ed25eadd4f1c45689bfdc057bfd92e2e5386f1ce
cf <1850146276@qq.com>
2019-06-22 23:31:18 +0800

    cf3

--- apiService/src/main/java/cn/ddw/apiService/goodsService/serivce/CommodityService.java
+++ apiService/src/main/java/cn/ddw/apiService/goodsService/serivce/CommodityService.java
@@ -2,6 +2,8 @@ package cn.ddw.apiService.goodsService.serivce;
 
 import cn.ddw.apiService.goodsService.entity.Commodity;
 import cn.ddw.apiService.goodsService.entity.Details;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -11,24 +13,27 @@ import java.util.List;
  * @Description :
  * 商品服务asASsdadsa
  */
+@RequestMapping("/com")
 public interface CommodityService {
     /**
      * 根据Id查询商品
      * @return
      */
-
+    @RequestMapping("/getCom/{uid}")
     Commodity getCommodityById(Integer Id);
 
     /**
      * 查询所有商品
      * @return
      */
+    @RequestMapping("/get/all")
     List<Commodity> getCommodityAll();
 
     /**
      * 根据商品类型查找商品
      * @return
      */
+    @GetMapping("/goods/typeAll/{tid}")
     List<Commodity>getCommodityType(Integer t_id);
 
 
